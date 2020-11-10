fd717902c8aad5e5d52c2b6b8cdc85c281b5dc85
laurene michon <laurene.michon@gmail.com>
2020-11-10 16:02:13 +0100

    Read ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #CREATE
+  get "/articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  # READ
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show", as: :article
+  # UPDATE
+  # DELETE
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  new_article = Article.new(
+    title: Faker::Company.name,
+    content: Faker::Quote.matz,
+  )
+  new_article.save!
+end
